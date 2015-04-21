b9866a97dde472d1b53b068e08ef77b8c14fd76b
Haruo Fujiwara <haruo.fujiwara@hakuhodo.co.jp>
2015-04-21 09:09:57 +0900

    merge

---
+++ leaptest003/Assets/Scripts/GUIController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIController : MonoBehaviour {
+
+	[SerializeField]
+	public GUIContent content;
+
+	private GUIStyle style;
+
+
+
+	// Use this for initialization
+	private void Start () {
+		Debug.Log ("Launching GUI!");
+		style = new GUIStyle();
+		style.fontStyle = FontStyle.Bold;
+		style.normal.textColor = Color.black;
+	}
+	
+	// Update is called once per frame
+	private void OnGUI () {
+		var rect = new Rect (10, 10, 400, 30);
+		GUI.Label (rect, content, style);
+		rect.y = 40;
+		if (GUI.Button (rect, "RESET"))
+		{
+			content.text = "yo";
+		}
+	}
+}
---
+++ leaptest003/Assets/Scripts/SoundCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundCube : MonoBehaviour {
+
+	public Vector3 startPosition;
+	public GameObject soundcube;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = this.gameObject.transform.position;
+		soundcube = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
