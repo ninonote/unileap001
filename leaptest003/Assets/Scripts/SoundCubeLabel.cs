f54a0d9191e8d84a44ca724a606816a252f07919
Haruo Fujiwara <haruo.fujiwara@hakuhodo.co.jp>
2015-04-23 15:21:19 +0900

    Removed redundant files.

--- leaptest003/Assets/Scripts/SoundCubeLabel.cs
+++ leaptest003/Assets/Scripts/SoundCubeLabel.cs
@@ -7,15 +7,15 @@ public class SoundCubeLabel : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		labelPosition = new Vector3 (40, 45, -25);
-		iTween.FadeTo(this.gameObject, iTween.Hash ("alpha", 0, "time", .5f));
+		//labelPosition = new Vector3 (40, 45, -25);
+		//iTween.FadeTo(this.gameObject, iTween.Hash ("alpha", 0, "time", .5f));
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//var rotation = Quaternion.LookRotation(Vector3.up , Vector3.forward);
 		//var rotation = Quaternion.LookRotation(Vector3.forward);
-		this.transform.rotation = Quaternion.Euler (90, 180, 0);
+		//this.transform.rotation = Quaternion.Euler (90, 180, 0);
 		//transform.rotation = rotation;
 		//this.transform.localPosition = labelPosition;
 	}
